a03b83eebda8b02394b8c02adcdfbea5f9f32863
OwaisAslam911 <owaisaslams911@gmail.com>
2025-10-20 10:55:01 -0700

    owais

--- ReactJs/Full_Stack_Crud_App/src/Components/Home/Home.jsx
+++ ReactJs/Full_Stack_Crud_App/src/Components/Home/Home.jsx
@@ -4,7 +4,25 @@ const Home = () => {
   return (
     <>
     <Navbar />
-    <div>Home</div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
     </>
   )
 }
